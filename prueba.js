baf30cffc36af509c5aa9570582f9247419e0ef3
MarthaLatorre <87924066+MarthaLatorre@users.noreply.github.com>
2021-07-27 17:06:43 -0500

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function exam(code,name,cfu,score){
+  this.code = code;
+  this.name = name;
+  this.cfu = cfu;
+  this.score = score;
+  }
+  const ma1=new exam ('1fg','web app',6,28);
+  console.log(ma1);
