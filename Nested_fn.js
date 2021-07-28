4d1681e06879adbb872ca28d9399c250dd01b232
MarthaLatorre <87924066+MarthaLatorre@users.noreply.github.com>
2021-07-28 12:08:56 -0500

    Function Hypotenuse
    nested function to calculate the hypotenuse with the legs

---
+++ Nested_fn.js
@@ -0,0 +1,11 @@
+function hypotenuse(a, b) {
+    const square = x => x*x ;
+    return  Math.sqrt (square(a) + square(b));
+    }
+
+    console.log(hypotenuse(2,3)) 
+    //    function hypotenuse(a, b) {
+    //    function
+    //    square(x) { return x*x; }
+    //    return Math.sqrt (square(a) + square(b));
+    //    }
